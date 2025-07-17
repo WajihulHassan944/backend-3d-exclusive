78043c8a23c49606b99e812c8506049230355e10
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-07-17 13:24:56 +0500

    updated code

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000", "http://localhost:3001"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000", "http://localhost:3001", "https://frontend-3d-exclusive.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
