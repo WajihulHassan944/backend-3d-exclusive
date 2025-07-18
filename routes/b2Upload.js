5fa0d4f580a4281535e25583be4f8e69d08261b8
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-07-18 09:50:27 +0500

    updated code

--- routes/b2Upload.js
+++ routes/b2Upload.js
@@ -6,7 +6,7 @@ import { b2upload } from '../middlewares/b2upload.js';
 const router = express.Router();
 
 router.post('/upload', isAuthenticated, b2upload.single('file'), uploadToB2);
-router.get('/uploads', getAllUploads);
+router.get('/uploads-forme', getAllUploads);
 router.get('/videos',authenticateApiKey, getAllUploadsAuthenticated);
 router.delete('/uploads/:id', deleteUpload);
 export default router;
