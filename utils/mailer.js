5777963968e9aaa9f3efa1ccd88113f33eab01e2
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-12-16 18:14:54 +0500

    updated code

--- utils/mailer.js
+++ utils/mailer.js
@@ -5,11 +5,12 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,     // smtp.transip.email
-  port: Number(process.env.SMTP_PORT), // 587
-  secure: false,                   // MUST be false for STARTTLS
-  requireTLS: true,                // Enables STARTTLS
+  port: Number(process.env.SMTP_PORT), // 465
+  secure: true,                 
   auth: {
     user: process.env.SMTP_USER,   // noreply@xclusive3d.com
-    pass: process.env.SMTP_PASS,   // password client gave you
+    pass: process.env.SMTP_PASS,   // password 
   },
 });
+
+
