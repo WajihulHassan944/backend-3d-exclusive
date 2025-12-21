0f2dc8d7d78f51a73f60b8a4938fa78ea18be64f
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-12-21 13:16:31 +0500

    updated port

--- utils/emailTemplate.js
+++ utils/emailTemplate.js
@@ -9,7 +9,7 @@ const generateEmailTemplate = ({
     <div style="font-family:Arial, sans-serif; max-width:600px; margin:auto;background: linear-gradient(135deg, #00005d 0%, #0700d3 50%, #a000ff 100%); color:#fff; border-radius:12px; overflow:hidden; box-shadow:0 0 15px rgba(0,0,0,0.1);">
       
       <div style="text-align:center; padding:30px 20px 10px;">
-    
+        <img src="${logoUrl}" alt="Xclusive 3D Logo" style="width:130px;" />
         <h1 style="margin:15px 0 0; font-size:24px; font-weight:600; letter-spacing:1px;color: #fff;">${subject}</h1>
       </div>
 
@@ -18,6 +18,7 @@ const generateEmailTemplate = ({
       </div>
 
       <div style="text-align:center; margin-top:20px; padding-bottom:30px;">
+      <a href="${siteUrl}">  <img src="${logoUrl}" alt="Logo" style="width:95px;  margin-top:20px;" /> </a>
         <p style="font-size:13px; color:#bbb; margin-top:10px;">Powered by Xclusive 3D</p>
       </div>
     </div>
--- utils/mailer.js
+++ utils/mailer.js
@@ -5,7 +5,7 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,     // smtp.transip.email
-  port: 587, // 465
+  port: Number(process.env.SMTP_PORT), // 465
   secure: true,                 
   auth: {
     user: process.env.SMTP_USER,   // noreply@xclusive3d.com
